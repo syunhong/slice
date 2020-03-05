c6901b93a04de285793144bb7099c0ad5c7f0f21
Seong-Yun Hong <hong.seongyun@gmail.com>
2020-03-05 15:51:06 +0900

    Added a progress bar

--- R/slice.R
+++ R/slice.R
@@ -1,25 +1,59 @@
 # ------------------------------------------------------------------------------
 # slice()
 # 
-# Date: 2018-12-01
+# Description:
+# 
+# 
 # Author: Seong-Yun Hong (syhong@khu.ac.kr)
+#
+# Last updated: 2020-03-05
 # ------------------------------------------------------------------------------
-slice <- function(x, at, vars) {
-  if (missing(vars))
-    vars <- names(x[[1]]@info)
-  
-  output <- lapply(x, FUN = function(z) {
-    info.df <- as.data.frame(z@info, stringsAsFactors = FALSE)
-    info.df <- info.df[vars]
+slice <- function(x, at, vars, showProgress = TRUE, na.rm = TRUE, 
+                  silent = FALSE) {
+
+  # ----------------------------------------------------------------------------
+  # Is 'x' a valid ASpaces object?
+  # ----------------------------------------------------------------------------
+  if (!inherits(x, "ASpaces"))
+    stop("'x' must be of class ASpaces", call. = FALSE)
+  else
+    df <- slot(x, "data")
+
+  # ----------------------------------------------------------------------------
+  # If 'at' is not given, stop the function
+  # ----------------------------------------------------------------------------
+  if (missing(at))
+    stop("'at' must be provided", call. = FALSE)
     
-    trip.df <- .locate(z@trip, at)
-    output <- cbind(info.df, trip.df)
-    return(output)    
-  })
+  # ----------------------------------------------------------------------------
+  # If 'vars' are not specified, return all variables in the input data
+  # ----------------------------------------------------------------------------
+  if (missing(vars))
+    vars <- names(slot(df[[1]], "info"))
+
+  if (showProgress & require(pbapply))
+    output <- pblapply(df, FUN = function(z, vars) {
+      info <- as.data.frame(z@info, stringsAsFactors = FALSE)
+      trip <- .locate(z@trip, info$id, at, na.rm, silent)
+      output <- cbind(info[vars], trip)
+      return(output)
+      })
+  else
+    output <- lapply(df, FUN = function(z, vars) {
+      info <- as.data.frame(z@info, stringsAsFactors = FALSE)
+      trip <- .locate(z@trip, info$id, at, na.rm, silent)
+      output <- cbind(info[vars], trip)
+      return(output)
+    })
+
+  output.unlist <- unlist(output)
+  output.length <- length(vars) + 4
+
+  output.df <- data.frame(matrix(output.unlist, ncol = output.length, 
+                                 byrow = TRUE), 
+                          stringsAsFactors = TRUE)
+
+  names(output.df) <- names(output[[1]])
   
-  result.df <- data.frame(matrix(unlist(output), ncol = 19, byrow = TRUE), 
-                          stringsAsFactors = FALSE)
-  names(result.df) <- names(output[[1]])
-  
-  return(result.df)
+  return(output.df)
 }
