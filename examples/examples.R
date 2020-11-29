6d6a091a6220f89e92b12c938fb8642351c90618
locklocke <hihi7100@khu.ac.kr>
2020-11-30 05:21:54 +0900

    change examples in package and add sampledata

--- examples/examples.R
+++ examples/examples.R
@@ -5,27 +5,45 @@
 # ------------------------------------------------------------------------------
 library(sp)
 library(rgdal)
+library(trajectories)
+library(spacetime)
 
+source("R/as.Tracks.R")
+source("R/as.TracksCollection.R")
 source("R/ASpace-class.R")
 source("R/ASpaces-class.R")
 source("R/ASpaces-methods.R")
-
-source("R/internals.R")
+source("R/get_xy.R")
+source("R/locate.R")
+source("R/locate.R")
+source("R/select.R")
 source("R/slice.R")
+source("R/slice2df.R")
+source("R/subset.R")
 
-load("d:/dev/slice_data/weekdays10.RData")
-load("d:/dev/slice_data/seoul_sp.RData")
-# seoul.sp <- readOGR("d:/dev/slice_data/seoul.shp", stringsAsFactors = FALSE,
-#                     encoding = "windows-949")
+load("examples/sampledata.RData")
 
-weekdays10.sp <- update(weekdays10, sp = seoul.sp)
+##example of simple slice function
+slice(testASP, 1200)
 
-dist_0900 <- slice(weekdays10.sp@data, at = 900)
-dist_1300 <- slice(weekdays10.sp@data, at = 1300)
-dist_1800 <- slice(weekdays10.sp@data, at = 1800)
+##example of as.TracksCollection
+testTC <- as.tracksCollection(testASP) ##without any argument
 
+testTC <- as.tracksCollection(testASP, varname = "adm_dr_cd") ##without sp
 
+testTC <- as.tracksCollection(testASP, testASP@sp, varname = "adm_dr_cd")
 
-x <- as.segdata(tmp$area, tmp$income)
-weekdays10.subset <- update(weekdays10.sp, data = weekdays10.sp@data[1:100])
+##example of as.TracksCollection with point data
+testtrip <- data.frame(tr_id = 1:10, tr_seq = rep(NA, 10), purpose = rep(NA, 10),
+                      mode = rep(NA, 10), o_type = rep(NA, 10), 
+                      o_time = c(200,300,400,500,600,700,800,900,1000,1100),
+                      o_zone = c(1,2,3,4,5,6,5,4,3,2),
+                      d_type = rep(NA, 10),
+                      d_time = c(300,400,500,600,700,800,900,1000,1100,1200),
+                      d_zone = c(2,3,4,5,6,5,4,3,2,1))
 
+testaspace <- new("ASpace", info = list(id = 1), trip = testtrip)
+
+pointaspaces <- new("ASpaces", data = list(testaspace), sp = testshp)
+
+as.tracksCollection(pointaspaces, varname = "adress")
