8f941f62dfd431a44198e418af19abb13d22f720
locklocke <hihi7100@khu.ac.kr>
2020-11-30 05:20:10 +0900

    add some code in as.tracksCollection and update comments

--- R/as.tracksCollection.R
+++ R/as.tracksCollection.R
@@ -15,9 +15,10 @@
 # 
 #   x             an object of class ASpaces
 #
-#   sp            
+#   sp            an object of class sp used in x
 #
-#   varname       
+#   varname       name of the column in sp's data.frame which can be matched 
+#                 with o_zone and d_zone in ASpaces
 #
 # Details:
 #
@@ -32,8 +33,24 @@
 # ------------------------------------------------------------------------------
 as.tracksCollection <- function(x, sp, varname) {
 
+  # ----------------------------------------------------------------------------
+  # If 'varname' is not given, stop the function
+  # ----------------------------------------------------------------------------
+  if (missing(varname))
+    stop("'varname' must be provided", call. = FALSE)
+  
+  # ----------------------------------------------------------------------------
+  # If 'sp' is missing, take the sp from given ASpaces "x"
+  # ----------------------------------------------------------------------------
+  if (missing(sp)){
+    sp <- x@sp
+  warning("Since sp is not given, the spatial data in x is used", 
+          call. = FALSE)
+  }
+  
   tracks_list <- lapply(x@data, function(z) try(as.Tracks(z, sp, varname), TRUE))
   errorID <- sapply(tracks_list, function(z) class(z) == "try-error")
   
   return(list(tracks_list, errorID))
 }
+
