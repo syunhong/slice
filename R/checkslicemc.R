6c0ffe9f099b938e7654ae2c625091aa36484e48
locklocke <hihi7100@khu.ac.kr>
2020-12-11 20:19:36 +0900

    added new functions .checkslicemc()

---
+++ R/checkslicemc.R
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+# .checkslicemc
+##
+# an internal function written by:
+#
+# Changlock Choi (hihi7100@khu.ac.kr)
+# ------------------------------------------------------------------------------
+
+.checkslicemc <- function(core){
+  core <- as.integer(core)
+  if((class(core) != "integer") )
+    stop("'core' must be integer value of 1 length",
+         call. = FALSE)
+  if(!length(core) == 1)
+    stop("'core' must be integer value of 1 length",
+         call. = FALSE)
+  if(is.na(core))
+    stop("'core' must be integer value fo 1 length",
+         call. = FALSE)
+  if(core > detectCores()){
+    core <- detectCores() - 1
+  cat("number of core is higher than the given environment,
+core is replaced with other value (maximum core - 1)")
+  }
+  return(core)
+}
+
